10739493446e002ba7737d6ea8c8a755854a3853
omkaryadav4433 <yadavomkar3344@gmail.com>
2023-08-30 14:04:54 +0530

    krishimantra

---
+++ krishimantra_backend1 (2)/KrishiMantra/KrishiMantra/src/main/java/com/KrshiMantra/KrishiMantraApplication.java
@@ -0,0 +1,26 @@
+package com.KrshiMantra;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class KrishiMantraApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(KrishiMantraApplication.class, args);
+		
+//		@Bean
+//		public ModelMapper mymodelmapper() {
+//			ModelMapper mapper=new ModelMapper();
+//			mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+//			return mapper;
+		}
+
+
+	}
+
+
